7fdf8c2c3400c8e002f547736413dd418b3bec9c
Azcion <zak.klajderic@gmail.com>
2017-09-04 01:45:33 +0200

    Add Colors class

---
+++ Handsome/Source/Colors.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace Handsome.Source {
+
+	internal static class Colors {
+
+		public static readonly Color Error = Color.Red;
+
+		public static readonly Color Background = Color.White;
+		public static readonly Color BackgroundAlt = Color.WhiteSmoke;
+
+		public static readonly Color CellHeaderBack = Color.White;
+		public static readonly Color CellHeaderFore = Color.DimGray;
+		public static readonly Color CellHeaderSelectionBack = Color.White;
+		public static readonly Color CellHeaderSelectionFore = Color.DimGray;
+		public static readonly Color CellBack = Color.White;
+		public static readonly Color CellFore = Color.DimGray;
+		public static readonly Color CellBackAlt = Color.WhiteSmoke;
+		public static readonly Color CellForeAlt = Color.DimGray;
+		public static readonly Color CellSelectionBack = Color.Gainsboro;
+		public static readonly Color CellSelectionFore = Color.Black;
+		public static readonly Color CellSelectionBackAlt = Color.Gainsboro;
+		public static readonly Color CellSelectionForeAlt = Color.Black;
+		public static readonly Color CellErrorBack = Color.IndianRed;
+		public static readonly Color CellErrorFore = Color.White;
+
+		public static readonly Color Theme = Color.FromArgb(74, 202, 168);
+
+	}
+
+}
